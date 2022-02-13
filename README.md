618d91a1615ebc8809e039fd4e4820829dfa6a0c
Rishab Arora <29125087+grobo021@users.noreply.github.com>
2022-02-13 18:36:53 +0530

    unlicense

---
+++ README.md
@@ -0,0 +1,24 @@
+# Snap Nuke
+This script nukes the snap package manager from Ubuntu.
+
+**Note**: This has only been tested on Ubuntu 20.04 LTS, and may not work 
+as intended on other versions. I will not be responsible for any damage if this
+script breaks your pc.
+
+If you really want a snap-less Ubuntu that is guaranteed to not break,
+you may want to try [Pop!_OS](https://pop.system76.com/) or [Linux Mint](https://linuxmint.com/)
+
+If you are a server administrator, I highly advise against running this script, since it hasn't
+been tested on Ubuntu Server. Please use Debian Testing instead. It's basically normal Debian but
+the packages are more up to date. This is also what Ubuntu uses as a baseline to build on top of.
+
+## Run the script
+```bash
+git clone https://github.com/grobo021/snap-nuke/
+cd snap-nuke
+./snap-nuke.sh
+```
+
+## Legal
+This repository is under the [unlicense](LICENSE.txt) license. You are free to use this however you want,
+but we are NOT providing any warranty that the script wont break the system. You have been warned.
--- snap-nuke.sh
+++ snap-nuke.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
+if [[ "$EUID" == 0 ]]
+then 
+cat << EOF
+┌──────────────────────────────────────────────────────────────────────┐
+│Please don't run this script as root as it may break you system.      │
+│We will ask you for the password if we need root access.              │
+└──────────────────────────────────────────────────────────────────────┘
+┬─┬ ノ( ゜-゜ノ)
+EOF
+exit
+fi
+
+cat << EOF
+┌──────────────────────────────────────────────────────────────────────┐
+│This Bash Script is made by u/Techno021 to nuke snaps from Ubuntu.    │
+│                                                                      │
+│Note: This has only been tested on Ubuntu 20.04 LTS, and may not work │
+│as intended on other versions. I will not be responsible for any      │
+│damage if this script breaks your pc                                  │
+└──────────────────────────────────────────────────────────────────────┘
+EOF
+
+while true; do
+    read -p "Do you wish to run the script? " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
 sudo snap remove $(snap list | awk '!/^Name|^core|^bare|^snapd/ {print $1}')
 sudo snap remove $(snap list | awk '/^bare/ {print $1}')
 sudo snap remove $(snap list | awk '/^core/ {print $1}')
 sudo snap remove $(snap list | awk '/^snapd/ {print $1}')
 
-for VARIABLE in $(df -h | awk '/snap/ {print $6}')
+for I in $(df -h | awk '/snap/ {print $6}')
 do
-        sudo umount $VARIABLE
+    sudo umount $I
 done
 
 sudo cp nosnap.pref /etc/apt/preferences.d/nosnap.pref
@@ -16,3 +47,12 @@ rm -rf ~/snap
 sudo rm -rf /snap
 sudo rm -rf /var/snap
 sudo rm -rf /var/lib/snapd
+
+cat << EOF
+┌─────────────────────────────────────────────────────────────────────┐
+│All Done! This script has succesfully completed, please reboot so    │
+│that changes take effect.                                            │
+└─────────────────────────────────────────────────────────────────────┘
+(╯°□°）╯︵ ┴─┴ 
+EOF
+echo
